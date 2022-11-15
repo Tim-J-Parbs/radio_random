91eeebb361729b7d7bbd839f3d6ace1f3425c0d7
Tim@Home <timparbs@gmail.com>
2022-11-15 01:55:04 +0100

    now async. I have to find ohut how to get around this mess

--- favorite_station.py
+++ favorite_station.py
@@ -9,8 +9,11 @@ from passwd_data import *
 parser = argparse.ArgumentParser(description='Favorite radio stations!')
 
 parser.add_argument('--radiourl', help="URL belonging to new favorite station", type=str, default=None)
+parser.add_argument('--db', help="Debug", type=int, default=0)
 input_args = parser.parse_args()
 radiourl = input_args.radiourl
+db = input_args.db
+
 if radiourl is None:
     print('No Radio url found :(')
     sys.exit(4)
@@ -38,24 +41,28 @@ async def add_fave():
                     pass
 
             for key, value in station_dict[i].items():
-                print('Key: ' + key)
-                print('Value type: ' + str(type(value)))
-                try:
-                    print(value)
-                except:
-                    print('Could not print value')
-                print('-----------------------')
+                if isinstance(value, list):
+                    station_dict[i][key] = tuple(value)
+                if db:
+                    print('Key: ' + key)
+                    print('Value type: ' + str(type(value)))
+                    try:
+                        print(value)
+                    except:
+                        print('Could not print value')
+                    print('-----------------------')
+
             new_favorite = pd.DataFrame.from_records(station_dict)
             try:
                 favorite_stations = pd.read_pickle(here  + "favorites.pickle")
                 print("Favorites found, opened database!")
-                favs = pd.concat([new_favorite, favorite_stations])
-                print("Concatenated databases.!")
-                favs.drop_duplicates()
             except Exception as e:
                 print(e)
                 print("No favorites found, building new database!")
-                favs = new_favorite
+                favorite_stations = new_favorite
+            favs = pd.concat([new_favorite, favorite_stations])
+            print("Concatenated databases.!")
+            favs.drop_duplicates()
             favs.to_pickle(here + "favorites.pickle")
             return
     print("Could not find radio url " + radiourl)
