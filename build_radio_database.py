e45a1ddc350683583d5ec07b40a5b3ec4351f2b1
Tim@Home <timparbs@gmail.com>
2023-04-20 23:21:49 +0200

    Added check for write access to database folder, removed superflous things, added small comments

--- build_radio_database.py
+++ build_radio_database.py
@@ -3,24 +3,31 @@ import pandas
 import asyncio
 import sys
 import re
-
+import os
 import numpy as np
 if 'win32' in sys.platform:
     # Windows specific event-loop policy & cmd
     asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
 
-dontsend = True
-
-debug = True
 here = sys.path[0]
+dir_suffix = 'countries'
+folderpath = here + '/' + dir_suffix
 allowed_codecs = ['MP3', 'AAC+', 'AAC', 'OGG']#, 'UNKOWN']
 async def async_build():
-    """Show example on how to query the Radio Browser API."""
-    async with RadioBrowser(user_agent="MyAwesomeApp/1.0.0") as radios:
+    """Build a local copy of the RadioBrowser database"""
+    try:
+        if not os.path.exists(folderpath):
+            os.makedirs(folderpath)
+    except Exception as e:
+        print('Unable to access or write to ' + folderpath)
+        print('Please check write permission of current user to that folder')
+    assert(os.access(folderpath, os.W_OK), 'Unable to write to ' + folderpath + ', please check permissions')
+
+    async with RadioBrowser(user_agent="random_radio/1.0.0") as radios:
         print('Building radio database from all over the world!')
         countries = await radios.countries()
-
         stations = await radios.stations()
+        # For each country, build a dictionary of stations, fix broken name strings and remove known bad codecs
         station_dict = [i.__dict__ for i in stations]
         for i in reversed(range(len(station_dict))):
             stationcodecs = station_dict[i]['codec'].split(',')
@@ -39,7 +46,7 @@ async def async_build():
         for thiscountry in unique_countries:
             associated_stations = df[df['country'] == thiscountry].copy()
             print("Created station dataframe for {} with {} recorded stations!".format(thiscountry, len(associated_stations)))
-            associated_stations.to_pickle(here + '/countries/' + re.sub('[^a-zA-Z0-9]', '', thiscountry) + '.pickle')
+            associated_stations.to_pickle(folderpath + '/' + re.sub('[^a-zA-Z0-9]', '', thiscountry) + '.pickle')
 
         return
 
@@ -48,4 +55,4 @@ def build():
     return
 
 if __name__ == "__main__":
-    asyncio.run(async_build())
+    build()
