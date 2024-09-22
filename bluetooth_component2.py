b8d40ec8fd6d33c67ee7e626de234e51d45fcdf3
Tim@Home <timparbs@gmail.com>
2024-09-22 22:52:26 +0200

    bugfix

--- bluetooth_component2.py
+++ bluetooth_component2.py
@@ -62,9 +62,7 @@ def list_devices():
     ]
     log = run_bluetoothctl(commands)
     mac_address = re.findall(r'Device ([0-9A-Fa-f]{2}(?::[0-9A-Fa-f]{2}){5})', log)[0]
-    print(log)
     devices = [d[0] for d in BT_DEVICES if d[1] == mac_address]
-    print(devices)
     return devices
 
 def disconnect_speaker(mac_address):
